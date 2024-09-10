82d11c476fefa11793450d4c24f39807c846d79c
Pedro Guaillas <peter.tufi@gmail.com>
2024-09-10 16:54:07 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        // your actual routes
+        Route::get('/', function () {
+            return view('welcome');
+        });
+
+        // Crear una empresa
+    });
+}
